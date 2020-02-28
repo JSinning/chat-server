4c487eded69bdbea74aef1db8d562587f7ee8744
JUAN DAVID CASTAÑO SINNING <redesloclaes@yahoo.com>
2020-02-28 16:41:17 -0500

    se prueba socket

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-//imports
+//imports 
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
+import http from 'http';
+import socketIO from 'socket.io';
+import { Socket } from 'dgram';
 
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 // Middleware
 app.use(morgan('tiny'));
 app.use(cors());
@@ -14,8 +19,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //routes
+io.on('connection', function(socket){
+    console.log("bienbenido");
 
+    socket.on('incremet', function(counter){
+        console.log("incremento");
+        io.sockets.emit('COUNTER_INCREMET', counter + 1);
+    });
 
+    socket.on('decremet', function(counter){
+        console.log("decremento");
+        io.sockets.emit('COUNTER_DECREMET', counter - 1);
+    });
+
+});
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');
 app.use(history());
